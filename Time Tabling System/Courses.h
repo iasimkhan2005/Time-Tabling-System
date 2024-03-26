8dc0aaa4c2b7732851d930a6354f2a050ba1879f
Muhammad Hashir <mehashir313@gmail.com>
2024-03-26 21:46:24 +0500

    Started filehandling

--- Time Tabling System/Courses.h
+++ Time Tabling System/Courses.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
 using namespace std;
@@ -13,4 +14,15 @@ public:
     string dayOfWeek;
     string startTime;
     string endTime;
+
+    // Function to write course details to a file
+    void writeToFile(ofstream &file) const
+    {
+        file << "Course ID: " << courseId << endl;
+        file << "Course Name: " << courseName << endl;
+        file << "Teacher: " << teacher.name << endl;
+        file << "Day: " << dayOfWeek << endl;
+        file << "Time: " << startTime << " - " << endTime << endl;
+        file << "------------------------\n";
+    }
 };
