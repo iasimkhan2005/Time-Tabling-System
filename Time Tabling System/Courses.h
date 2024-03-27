078fd66bcade3e73ee2432a7b0b3b4e033f4ae4b
iasimkhan2005 <asimali9596a@gmail.com>
2024-03-27 16:57:28 +0500

    update Course.h

--- Time Tabling System/Courses.h
+++ Time Tabling System/Courses.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
 class Courses
 {
 public:
@@ -9,5 +14,15 @@ public:
     string dayOfWeek;
     string startTime;
     string endTime;
-};
 
+    // Function to write course details to a file
+    void writeToFile(ofstream& file) const
+    {
+        file << "Course ID: " << courseId << endl;
+        file << "Course Name: " << courseName << endl;
+        file << "Teacher: " << teacher.name << endl;
+        file << "Day: " << dayOfWeek << endl;
+        file << "Time: " << startTime << " - " << endTime << endl;
+        file << "------------------------\n";
+    }
+};
