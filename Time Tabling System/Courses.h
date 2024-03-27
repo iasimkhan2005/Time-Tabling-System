42be81a0591235f5bee3c9959f5c8cb88e42d83d
Muhammad Hashir <mehashir313@gmail.com>
2024-03-27 22:16:22 +0500

    at the ending

--- Time Tabling System/Courses.h
+++ Time Tabling System/Courses.h
@@ -1,50 +1,58 @@
+#pragma once
 #include <iostream>
 #include <string>
 #include <vector>
 #include "Teachers.h"
 #include "Rooms.h"
 #include "Sections.h"
-#include "Time.h" 
-class Students; // Forward declaration of Student class
- 
+#include "Time.h"
+#include "Students.h"
+// class Students; // Forward declaration of Student class
+
 using namespace std;
 
-class Courses {
+class Courses
+{
 private:
     int courseCode;
     int studentCode;
     string courseName;
-    Rooms* assignedRoom;
-    Teachers* teacher;
-    Sections* assignedSection;
-    Time* assignedTime;
-    map<int, string>student_data;
+    Rooms *assignedRoom;
+    Teachers *teacher;
+    Sections *assignedSection;
+    Time *assignedTime;
+    map<int, string> student_data;
 
 public:
-    vector<Students*> enrolledStudents;
+    vector<Students *> enrolledStudents;
 
-    Courses(int courseCode, const string& courseName, Teachers* teacher, Rooms* assignedRoom) : courseCode(courseCode), courseName(courseName), teacher(teacher), assignedRoom(assignedRoom), assignedSection(nullptr), assignedTime(nullptr) {}
+    Courses(int courseCode, const string &courseName, Teachers *teacher, Rooms *assignedRoom) : courseCode(courseCode), courseName(courseName), teacher(teacher), assignedRoom(assignedRoom), assignedSection(nullptr), assignedTime(nullptr) {}
 
-    void addStudent(Students student) {
-        student_data[student.getStudentID()] =  studentCode;
-    }
-    void removeStudent(Students student) {
-		student_data[student.getStudentID()].erase();
-    }
+    // void addStudent(Students student)
+    // {
+    //     student_data[student.getStudentID()] = studentCode;
+    // }
+    // void removeStudent(Students student)
+    // {
+    //     student_data[student.getStudentID()].erase();
+    // }
 
-    string getCourseName() {
+    string getCourseName()
+    {
         return courseName;
     }
 
-	Teachers getTeacher() const {
-		return *teacher;
+    Teachers getTeacher() const
+    {
+        return *teacher;
     }
-    Rooms* getAssignedRoom() const;
-    Sections* getAssignedSection() const;
-    Time* getAssignedTime() const;
-    void setAssignedSection(Sections* section);
+    Rooms *getAssignedRoom() const;
+    Sections *getAssignedSection() const;
+    Time *getAssignedTime() const;
+    void setAssignedSection(Sections *section);
 
-    vector<Students*> getEnrolledStudents() const {
-		return enrolledStudents;
-	}
+    vector<Students *> getEnrolledStudents() const
+    {
+        return enrolledStudents;
+    }
 };
--- Time Tabling System/main.cpp
+++ Time Tabling System/main.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
-#include<map>
-#include<vector>
+#include <iostream>
+#include <map>
+#include <vector>
 #include "Students.h"
 #include "Teachers.h"
 #include "Courses.h"
@@ -9,42 +9,69 @@
 #include "Sections.h"
 #include "Time.h"
 using namespace std;
-                                    //Functions
+// Functions
 void menu();
 
 int main()
 {
-	                               
-	                              //STUDENTS INFO	                             
-	Students student1(59, "Hashir", "01-131232-059@bahria.com", "BSE-2A");
-	Students student2(15, "Asim", "01-131232-015@bahria.com", "BSE-2A"); 
-	                                
-	                                //TEACHERS INFO
-	Teachers* teacher1 = new Teachers("SIR-Tamin", 101, "Tamin123@bahria.com");
-	Teachers* teacher2 = new Teachers("SIR-Waleed", 102, "Waleed123@bahria.com");
-	                                  
-	                                 //ROOMS INFO
-	Rooms room1("4-17", 50);
-	Rooms room2("4-18", 50);
-	Rooms room3("4-19", 50);
 
-	                                  //COURSES INFO
-    Courses course1(131, "OOP", teacher1, &room1);	
-	Courses course2(232, "OOP - Lab ", teacher2, &room2);
+    // STUDENTS INFO
+    Students student1(59, "Hashir", "01-131232-059@bahria.com", "BSE-2A");
+    Students student2(15, "Asim", "01-131232-015@bahria.com", "BSE-2A");
 
-	                                 //REGISTRATION INFO
-	course1.addStudent(student1);
-	course1.addStudent(student2);
-	course2.addStudent(student1);
-	course2.addStudent(student2);
+    // TEACHERS INFO
+    Teachers *teacher1 = new Teachers("SIR-Tamin", 101, "Tamin123@bahria.com");
+    Teachers *teacher2 = new Teachers("SIR-Waleed", 102, "Waleed123@bahria.com");
 
-	                                  // MAIN MENU
-	void menu();
-	return 0;
+    // ROOMS INFO
+    Rooms room1("4-17", 50);
+    Rooms room2("4-18", 50);
+    Rooms room3("4-19", 50);
+
+    // COURSES INFO
+    Courses course1(131, "OOP", teacher1, &room1);
+    Courses course2(232, "OOP - Lab ", teacher2, &room2);
+
+    // REGISTRATION INFO
+    // course1.addStudent(student1);
+    // course1.addStudent(student2);
+    // course2.addStudent(student1);
+    // course2.addStudent(student2);
+
+    // MAIN MENU
+    void menu();
+    return 0;
 }
 
-void menu() {
+void menu()
+{
+    char choice;
+    Timetable timetable;
 
+    cout << "\t\tWelcome to Bahria University Time Tabling System\n";
+    cout << "\t\tPlease select an option from the following list:\n";
+    cout << "\t\t1. Teacher Wise Timetable.\n";
+    cout << "\t\t2. Student Wise Timetable.\n";
+    cout << "\t\t3. Section Wise Timetable.\n";
+    cout << "\t\t4. Queries Wise Timetable\n";
+    cout << "\t\t5. Exit\n";
+    cout << "\t\tEnter your choice: ";
+    cin >> choice;
+    switch (choice)
+    {
+    case '1':
+        system("cls");
+        timetable.teacherTimetable();
+        break;
 
+        // case '2':
+        //     system("cls");
+        //     timetable.studentTimetable();
+        //     break;
 
+    case '3':
+        system("cls");
+        timetable.sectionTimetable();
+        break;
+    }
 }
